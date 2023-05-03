1ea4090749ae33b6a40284ffee5eec39e966198c
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-05-03 23:02:24 +0300

    feat(components): add component SideBar

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledSideBar, Copyright } from "./styles";
+
+export const SideBar = () => {
+  return (
+    <StyledSideBar>
+      <Copyright>© All Rights Reserved</Copyright>
+    </StyledSideBar>
+  );
+};
---
+++ src/components/SideBar/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Color, Typography } from "ui";
+
+export const StyledSideBar = styled.aside`
+  display: grid;
+  grid-template-rows: 40px 1fr 24px;
+  grid-row-gap: 64px;
+`;
+
+export const Copyright = styled.p`
+  ${Typography.B1};
+  color: ${Color.Light};
+  align-self: center;
+`;
