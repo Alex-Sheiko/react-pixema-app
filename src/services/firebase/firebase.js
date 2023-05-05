effccc209faa4f5afe4705dfb196c1c2ed535c1a
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-05-05 14:38:21 +0300

    feat(services): add firebase to app

---
+++ src/services/firebase/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQHGk0mcbGVPH2iD76VLa5lONj9XUpEE4",
+  authDomain: "react-pixema-481f5.firebaseapp.com",
+  projectId: "react-pixema-481f5",
+  storageBucket: "react-pixema-481f5.appspot.com",
+  messagingSenderId: "358533828865",
+  appId: "1:358533828865:web:b210dbbaeffc7bf5b0cb00",
+};
+
+const app = initializeApp(firebaseConfig);
