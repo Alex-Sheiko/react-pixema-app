b2a941e9163819a8e3ec70da170f3eee20403237
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-05-22 15:39:25 +0300

    feat(pages): add FavoritesPage data

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Feedback, Spinner, MovieCard } from "components";
+import { useEffect } from "react";
+import { fetchFavorites, getFavorites, useAppDispatch, useAppSelector } from "store";
+import { FavoriteList } from "./styles";
 
 export const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const { favorites, isLoading } = useAppSelector(getFavorites);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchFavorites());
+  }, [dispatch]);
+  if (isLoading) return <Spinner />;
+  if (!favorites.length) {
+    return <Feedback text="Favorites not found" />;
+  }
+  return (
+    <FavoriteList
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeIn", duration: 0.5 }}
+    >
+      {favorites &&
+        favorites.map(({ imdbID, title, poster, genres, imdbRating }) => (
+          <MovieCard
+            key={imdbID}
+            title={title}
+            img={poster}
+            id={imdbID}
+            genres={genres}
+            rating={imdbRating}
+            favorite
+          />
+        ))}
+    </FavoriteList>
+  );
 };
